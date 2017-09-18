ebbce6465d09f5091711e3e5599a77dbf92e3efb
sivaprasadraju <sraju728@gmail.com>
2017-09-18 18:44:27 +0530

    Adding UI using React.js

--- src/App.js
+++ src/App.js
@@ -7,35 +7,54 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>To Do List</h2>
+          <div className="h2color">
+            <h2 className="display-3">ToDoList</h2>
+          </div>
         </div>
         <hr className='ml-1' />
-        <form>
-          <div className="input-group mb-0 mr-sm-0 mb-sm-0">
-            Title:
-              <input type="text" className="form-control" id="text_title" />
-
-              <label className="mr-sm-2" for="inlineFormCustomSelect">Preferences</label>
-              <select className="custom-select mb-2 mr-sm-2 mb-sm-0" id="button_dropdown">
-                <option selected className="pri_1">High</option>
-                <option value="Medium" className="pri_2">Medium</option>
-                <option value="Low" className="pri_3">Low</option>
-              </select>
-  
-              <button type="button" className="btn btn-success my-2 my-sm-0" onclick="AddItems()">ADD</button>
-            </div>
-        </form>
-
-        <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-12">
-          s;
+      <form className="form-inline">
+        <div className="input-group mb-0 mr-sm-0 mb-sm-0">
+          <div className="input-group-addon">Title</div>
+          <input type="text" className="form-control" id="text_title" required />
         </div>
+
+        <label className="mr-sm-2" for="inlineFormCustomSelect">Priority</label>
+        <select className="custom-select mb-2 mr-sm-2 mb-sm-0" id="button_dropdown">
+          <option selected className="pri_1">High</option>
+          <option value="Medium" className="pri_2">Medium</option>
+          <option value="Low" className="pri_3">Low</option>
+        </select>
+          <button type="button" className="btn btn-success my-2 my-sm-0" onclick="AddItems()">ADD</button>
+      </form>
+
+        <div className="margin">
+          <div className="panel-body">
+            <div className="width">
+              <div className="table-responsive">
+                <table id="mytable" className="table table-striped table-bordered table-list">
+                  <thead>
+                    <tr className="text-center">
+                      <th className="text-center">Done</th>
+                      <th className="text-center">Title</th>
+                      <th className="text-center">Priority</th>
+                      <th className="text-center">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody id= "tbody" className="text-center"></tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <footer className="navbar-fixed-bottom">
+      <div className="footer_fixed">
+        <hr className="dotted" />
+        <span>Powered by Aconex</span>
       </div>
-    </div>
+    </footer>
+  </div>
 
-
-      
     );
   }
 }
